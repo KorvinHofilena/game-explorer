1e5c24606c0c1c8abec0b427a5a57556eefa43b2
Korvin <hofilenakorvin@gmail.com>
2025-06-18 05:38:01 -0700

    adjusted Profile/Home.jsx

---
+++ game-explorer-client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section>
+      <h1>Welcome to Game Explorer</h1>
+      <p>This is the home page.</p>
+    </section>
+  );
+}
+
+export default Home;
---
+++ game-explorer-client/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <section>
+      <h1>Your Profile</h1>
+      <p>This is your profile page.</p>
+    </section>
+  );
+}
+
+export default Profile;
