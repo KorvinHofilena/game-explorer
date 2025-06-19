5d07a6d6af084c9a46c35e5d807bc71dade6b114
Korvin <hofilenakorvin@gmail.com>
2025-06-18 17:14:01 -0700

    changed BrowserRouter to HashRouter in main.jsx

--- game-explorer-client/src/main.jsx
+++ game-explorer-client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { FavoritesProvider } from "./contexts/FavoritesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
