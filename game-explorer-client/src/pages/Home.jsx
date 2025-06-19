54bd52a549335b37b1e879f9612649818a442fcc
Korvin <hofilenakorvin@gmail.com>
2025-06-18 17:35:06 -0700

    adjusted sizing for homepage and profile

--- game-explorer-client/src/pages/Home.jsx
+++ game-explorer-client/src/pages/Home.jsx
@@ -7,22 +7,22 @@ import styles from "./Home.module.css";
 function Home() {
   const [games, setGames] = useState([]);
   const { addFavorite } = useContext(FavoritesContext);
-  const [loading, setLoading] = useState(true); // 🔹 NEW
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchPopularGames().then((data) => {
       setGames(data);
-      setLoading(false); // 🔹 NEW
+      setLoading(false);
     });
   }, []);
 
   return (
-    <section>
+    <section className={styles.wrapper}>
       <h1>Popular Games</h1>
-      {loading ? ( // 🔹 UPDATED condition
+      {loading ? (
         <div className={styles.spinner}></div>
       ) : (
-        <div className={styles["game-grid"]}>
+        <div className={styles.gameGrid}>
           {games.map((game) => (
             <GameCard key={game.id} game={game} onSave={addFavorite} />
           ))}
